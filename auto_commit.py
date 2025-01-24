9c720dd3a49e342b6607019f16f44d451a53c80c
dakota.rennels <dakotarennels@gmail.com>
2025-01-24 14:25:46 -0600

    Adding new auto_commit code

--- auto_commit.py
+++ auto_commit.py
@@ -6,12 +6,11 @@ import schedule
 REPO_PATH = os.getcwd()  # Automatically detect the repo folder
 COUNTER_FILE = os.path.join(REPO_PATH, 'counter.txt')
 
-def initialize_counter():
-    if not os.path.exists(COUNTER_FILE):
-        with open(COUNTER_FILE, 'w') as f:
-            f.write('0')
+COMMIT_MESSAGE_TEMPLATE = 'Auto-increment counter to {counter}'
 
+# Increment the counter and push changes
 def commit_and_push():
+    # Increment the counter
     with open(COUNTER_FILE, 'r+') as f:
         current_value = int(f.read().strip())
         new_value = current_value + 1
@@ -19,23 +18,10 @@ def commit_and_push():
         f.write(str(new_value))
         f.truncate()
 
-    os.system(f'cd {REPO_PATH} && git add counter.txt')
-    os.system(f'cd {REPO_PATH} && git commit -m \"Update file with new code and new value: {new_value}\"')
+    # Git commands
+    os.system(f'cd {REPO_PATH} && git add {COUNTER_FILE}')
+    os.system(f'cd {REPO_PATH} && git commit -m \"{COMMIT_MESSAGE_TEMPLATE.format(counter=new_value)}\"')
     os.system(f'cd {REPO_PATH} && git push')
 
-def schedule_daily_commits():
-    random_commits = random.randint(20, 30)
-    interval = int(24 * 60 / random_commits)
-
-    print(f"Scheduled {random_commits} commits for today (every {interval} minutes).\")
-
-    for _ in range(random_commits):
-        schedule.every(interval).minutes.do(commit_and_push)
-
 if __name__ == "__main__":
-    initialize_counter()
-    schedule_daily_commits()
-
-    while True:
-        schedule.run_pending()
-        time.sleep(1)
+    commit_and_push()
